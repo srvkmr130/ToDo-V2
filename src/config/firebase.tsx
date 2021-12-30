e6e199ac426a24eefa1f1a6623832e59b1d5f12c
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2021-12-30 18:32:37 +0530

    Update firebase.tsx

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWE9rlDJUXe452ja6TND1xWz86gdPUUFA",
-  authDomain: "todo-app-db743.firebaseapp.com",
-  projectId: "todo-app-db743",
-  storageBucket: "todo-app-db743.appspot.com",
-  messagingSenderId: "297459420402",
-  appId: "1:297459420402:web:32602e3629a7fe320e146b",
-  measurementId: "G-71WWR1YK29",
+  apiKey: "<apiKey>",
+  authDomain: "<authDomain>",
+  projectId: "<projectId>",
+  storageBucket: "<storageBucket>",
+  messagingSenderId: "<messagingSenderId>",
+  appId: "<appId>",
+  measurementId: "<measurementId>",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
